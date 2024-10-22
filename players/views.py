82f70afea2e5d7c96d8971e170f6646afdff99d0
sajjadhossein <sajjadhosseinrezaei@yahoo.com>
2024-10-22 18:15:17 +0330

    add ListView

--- players/views.py
+++ players/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Players
 # Create your views here.
+
+
+class PlayersListView(ListView):
+    model = Players
+    template_name = 'players/players_list.html'
+    context_object_name = 'players'
+
